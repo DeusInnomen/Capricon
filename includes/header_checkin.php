c9b9f55ad3d65ea513982c2e2cb1c811bb1a4d07
Paul Timmins <paul@timmins.net>
2021-09-11 18:33:37 -0400

    bad merge

--- includes/header_checkin.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	include_once('includes/functions.php');
-?>
-	<div class="header">
-		<div class="posLeft"><img src="includes/CapReg.png" /></div>
-		<div class="posRight"><img src="includes/capricious.png" /></div>
-	</div>
